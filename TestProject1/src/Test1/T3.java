605c78cb0079e464b6dc1bad1977df88ab8169bf
SONY <SONY@SONY-PC>
2023-02-19 17:48:40 +0530

    Commiting T3.java

---
+++ TestProject1/src/Test1/T3.java
@@ -0,0 +1,20 @@
+package Test1;
+
+public class T3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=110;
+		
+		if(i<=10) {
+			
+			System.out.println("Hi");
+		}
+
+		else {
+			System.out.println("Hello");
+		}
+	}
+
+}
